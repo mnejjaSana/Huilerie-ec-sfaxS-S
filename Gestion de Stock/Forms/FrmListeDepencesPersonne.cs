85ac6c05babfe50ff55256a6b17e993251fc8c26
MnejjaSana <sana.mnejja@groupe-ec.com>
2024-10-07 16:31:41 +0100

    update

---
+++ Gestion de Stock/Forms/FrmListeDepencesPersonne.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace Gestion_de_Stock.Forms
+{
+    public partial class FrmListeDepencesPersonne : DevExpress.XtraEditors.XtraForm
+    {
+        public FrmListeDepencesPersonne()
+        {
+            InitializeComponent();
+        }
+    }
+}
